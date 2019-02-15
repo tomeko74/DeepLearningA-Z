cb4213b7acb17c2a9efe90d56d66d9e8bf9bd5bf
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-15 15:54:01 +0100

    fixes

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn_homework_solution.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn_homework_solution.py
@@ -67,9 +67,25 @@ test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = classifier.predict(test_image)
 
-training_set.class_indices
+print(training_set.class_indices)
 
 if result[0][0] == 1:
     prediction = 'dog'
 else:
     prediction = 'cat'
+
+print(f'dataset/single_prediction/cat_or_dog_1.jpg is {prediction}')
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = classifier.predict(test_image)
+
+print(training_set.class_indices)
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+print(f'dataset/single_prediction/cat_or_dog_2.jpg is {prediction}')
