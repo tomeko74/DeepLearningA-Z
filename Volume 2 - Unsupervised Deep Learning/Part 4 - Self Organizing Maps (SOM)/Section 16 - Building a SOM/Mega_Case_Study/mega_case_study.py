61b046cf5a054e5130dacdee4d84f86a54828aa0
tomeko74 <thomasz.orlowski@gmail.com>
2019-03-17 20:25:13 +0100

    fixes

--- Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Mega_Case_Study/mega_case_study.py
+++ Volume 2 - Unsupervised Deep Learning/Part 4 - Self Organizing Maps (SOM)/Section 16 - Building a SOM/Mega_Case_Study/mega_case_study.py
@@ -16,14 +16,14 @@ y = dataset.iloc[:, -1].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+sc = MinMaxScaler(feature_range=(0, 1))
 X = sc.fit_transform(X)
 
 # Training the SOM
 from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som = MiniSom(x=10, y=10, input_len=15, sigma=1.0, learning_rate=0.5)
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+som.train_random(data=X, num_iteration=100)
 
 # Visualizing the results
 from pylab import bone, pcolor, colorbar, plot, show
@@ -37,15 +37,15 @@ for i, x in enumerate(X):
     plot(w[0] + 0.5,
          w[1] + 0.5,
          markers[y[i]],
-         markeredgecolor = colors[y[i]],
-         markerfacecolor = 'None',
-         markersize = 10,
-         markeredgewidth = 2)
+         markeredgecolor=colors[y[i]],
+         markerfacecolor='None',
+         markersize=10,
+         markeredgewidth=2)
 show()
 
 # Finding the frauds
 mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
+frauds = np.concatenate((mappings[(5, 3)], mappings[(8, 3)]), axis=0)
 frauds = sc.inverse_transform(frauds)
 
 
@@ -58,7 +58,7 @@ customers = dataset.iloc[:, 1:].values
 # Creating the dependent variable
 is_fraud = np.zeros(len(dataset))
 for i in range(len(dataset)):
-    if dataset.iloc[i,0] in frauds:
+    if dataset.iloc[i, 0] in frauds:
         is_fraud[i] = 1
 
 # Feature Scaling
@@ -76,18 +76,18 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+classifier.add(Dense(units=2, kernel_initializer='uniform', activation='relu', input_dim=15))
 
 # Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
 # Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+classifier.fit(customers, is_fraud, batch_size=1, epochs=2)
 
 # Predicting the probabilities of frauds
 y_pred = classifier.predict(customers)
-y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis=1)
 y_pred = y_pred[y_pred[:, 1].argsort()]
